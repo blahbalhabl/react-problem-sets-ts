312bfbfc2b96da9bb0d0a9c73a8a15554e648bd3
Mark Leigh David <it.markleighdavid@gmail.com>
2024-06-25 18:03:21 +0800

    chore: added-problem-routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/HomePage';
+// Beginner Problems
 import Problem_1 from './pages/beginner/Problem_1';
+import Problem_2 from './pages/beginner/Problem_2';
+import Problem_3 from './pages/beginner/Problem_3';
+import Problem_4 from './pages/beginner/Problem_4';
+import Problem_5 from './pages/beginner/Problem_5';
 
 const App = () => {
   return (
@@ -14,6 +19,22 @@ const App = () => {
           <Route
             path='/beginner/problem_1'
             element={<Problem_1 />}
+          />
+          <Route
+            path='/beginner/problem_2'
+            element={<Problem_2 />}
+          />
+          <Route
+            path='/beginner/problem_3'
+            element={<Problem_3 />}
+          />
+          <Route
+            path='/beginner/problem_4'
+            element={<Problem_4 />}
+          />
+          <Route
+            path='/beginner/problem_5'
+            element={<Problem_5 />}
           />
           {/* <Route path="/contact" element={<Contact />} /> */}
         </Routes>
